1aa28844531615422806a0dcf472d481ae2c1e2b
Saurabh-1209 <121961770+Saurabh-1209@users.noreply.github.com>
2023-01-06 19:10:23 +0530

    Add files via upload

---
+++ Mex of 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b,c=0;
+    printf("Enter two non-negative integers: ");
+    scanf("%d %d",&a,&b);
+    if(a>0 && b>0){
+        printf("%d",c);
+    }
+    if(a==0 || b==0){
+        if(a==1 || b==1){
+            printf("%d",c+2);
+        }
+    }
+
+    return 0;
+}
